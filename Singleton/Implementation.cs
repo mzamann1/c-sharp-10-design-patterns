b8dd1302da45638ff38ff763f91da70be906799e
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-10-20 18:40:29 +0500

    added comments in singleton design pattern.

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -2,14 +2,23 @@
 
 public class Logger
 {
-    //Lazy<T>
+    //Lazy<T> for thread safety
+
+    /// <summary>
+    /// Provides support for lazy initialization.
+    /// By default, all public and protected members of <see cref="Lazy{T}">; are thread-safe
+    /// and may be used concurrently from multiple threads.
+    /// </summary>
+
     private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
 
 
+    //property to be available for the clients to get instance of the class
     public static Logger Instance
     {
         get
         {
+            //returning the lazy value of logger
             return _lazyLogger.Value;
         }
     }
@@ -18,6 +27,7 @@ public class Logger
     {
     }
 
+    //simple method
     public void Log(string message)
     {
         Console.WriteLine($"Message from {message}");
